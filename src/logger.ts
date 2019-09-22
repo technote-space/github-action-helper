345ff0ab73f3d84f22cf73dca0f0cb1c2761c342
Technote <technote.space@gmail.com>
2019-09-23 00:47:02 +0900

    feat: refactor logger (#4, #5)

--- src/logger.ts
+++ src/logger.ts
@@ -1,64 +1,78 @@
-import { Signale } from 'signale';
-import types from 'signale/types';
+import { EOL } from 'os';
+import { sprintf } from 'sprintf-js';
 
 /**
  * Logger
  */
 export class Logger {
 
-	private readonly signale: Signale;
 	private readonly replacer: (string) => string;
 	private static isRequiredEndGroup = false;
 
 	/**
 	 * @param {function|undefined} replacer replacer
-	 * @param {object|undefined} signaleSettings signale settings
 	 */
-	constructor(replacer?: (string) => string, signaleSettings?: object) {
-		this.signale = new Signale(Object.assign({}, {
-			types: {
-				info: {
-					color: 'cyan',
-				},
-			},
-		}, signaleSettings));
-
+	constructor(replacer?: (string) => string) {
 		this.replacer = replacer ? replacer : (text: string): string => text;
-
-		Object.keys(types).forEach(type => {
-			if (!this[type]) {
-				this[type] = this.signale[type];
-			}
-		});
 	}
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	private output = (type: 'log' | 'info' | 'warn', message: string, ...args: any[]): void => {
-		this.signale[type](this.replacer(message), ...args.map(arg => 'string' === typeof arg ? this.replacer(arg) : arg));
+	/**
+	 * @param {string} message message
+	 * @return {string[]} message
+	 */
+	private splitMessage = (message: string): string[] => message.replace(/\r?\n$/, '').split(/\r?\n/);
+
+	/**
+	 * @param {string} message message
+	 * @param {any[]} args args
+	 */
+	public log = (message: string, ...args: any[]): void => { // eslint-disable-line @typescript-eslint/no-explicit-any
+		process.stdout.write(sprintf(this.replacer(message), ...args.map(arg => 'string' === typeof arg ? this.replacer(arg) : arg)) + EOL);
 	};
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public info = (message: string, ...args: any[]): void => this.output('info', message, ...args);
+	private multiLineOutput = (replacer: (string) => string, message: string | string[], ...args: any[]): void => {
+		if ('string' !== typeof message) {
+			message.forEach(message => {
+				this.multiLineOutput(replacer, message, ...args);
+			});
+			return;
+		}
+
+		this.splitMessage(message).forEach(message => this.log(replacer(message), ...args));
+	};
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public displayCommand = (message: string, ...args: any[]): void => this.output('log', `[command]${message}`, ...args);
+	public info = (message: string | string[], ...args: any[]): void => this.multiLineOutput(message => `> ${message}`, message, ...args);
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public displayStdout = (message: string): void => message.replace(/\r?\n$/, '').split(/\r?\n/).forEach(line => this.output('log', `  >> ${line}`));
+	public debug = (message: string | string[], ...args: any[]): void => this.multiLineOutput(message => `##[debug]${message}`, message, ...args);
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public displayStderr = (message: string): void => message.replace(/\r?\n$/, '').split(/\r?\n/).forEach(line => this.output('warn', `  >> ${line}`));
+	public error = (message: string | string[], ...args: any[]): void => this.multiLineOutput(message => `##[error]${message}`, message, ...args);
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public warn = (message: string | string[], ...args: any[]): void => this.multiLineOutput(message => `##[warning]${message}`, message, ...args);
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public displayCommand = (message: string | string[], ...args: any[]): void => this.multiLineOutput(message => `[command]${message}`, message, ...args);
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public displayStdout = (message: string | string[]): void => this.multiLineOutput(message => `  >> ${message}`, message);
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public displayStderr = (message: string | string[]): void => this.multiLineOutput(message => `##[warning]  >> ${message}`, message);
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	public startProcess = (message: string, ...args: any[]): void => {
 		this.endProcess();
-		this.output('log', `##[group]${message}`, ...args);
+		this.log(`##[group]${message}`, ...args);
 		Logger.isRequiredEndGroup = true;
 	};
 
 	public endProcess = (): void => {
 		if (Logger.isRequiredEndGroup) {
-			this.output('log', '##[endgroup]');
+			this.log('##[endgroup]');
 			Logger.isRequiredEndGroup = false;
 		}
 	};
@@ -66,29 +80,4 @@ export class Logger {
 	public static resetForTesting = (): void => {
 		Logger.isRequiredEndGroup = false;
 	};
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type LoggerFunction = (...message: any[]) => void;
-type DefaultLogger =
-	| 'await'
-	| 'complete'
-	| 'debug'
-	| 'error'
-	| 'fatal'
-	| 'fav'
-	| 'info'
-	| 'log'
-	| 'note'
-	| 'pause'
-	| 'pending'
-	| 'star'
-	| 'start'
-	| 'success'
-	| 'wait'
-	| 'warn'
-	| 'watch'
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Logger extends Record<DefaultLogger, LoggerFunction> {
 }
