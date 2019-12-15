0808ff9a08a326bbb0b27f1c160212edbd3bcef4
Technote <technote.space@gmail.com>
2019-12-16 01:27:50 +0900

    feat: options for git fetch

--- src/git-helper.ts
+++ src/git-helper.ts
@@ -262,14 +262,16 @@ export default class GitHelper {
 	/**
 	 * @param {string} workDir work dir
 	 * @param {Context} context context
+	 * @param {string[]} options options
 	 * @param {string[]} refspec refspec
 	 * @return {Promise<void>} void
 	 */
-	public fetchOrigin = async(workDir: string, context: Context, refspec?: string[]): Promise<void> => {
+	public fetchOrigin = async(workDir: string, context: Context, options?: string[], refspec?: string[]): Promise<void> => {
 		await this.addOrigin(workDir, context);
 		await this.runCommand(workDir, {
 			command: 'git fetch',
 			args: [
+				...(options ?? []),
 				'origin',
 				...(refspec ?? []),
 			],
