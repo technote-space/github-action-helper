9660efd8126d17393fa88a821864a9a6975ecef4
Technote <technote.space@gmail.com>
2020-04-18 17:49:29 +0900

    chore: use export as syntax (#243)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Logger from './logger';
 import Command from './command';
 import ApiHelper from './api-helper';
 import GitHelper from './git-helper';
-import * as Utils from './utils';
-import * as ContextHelper from './context-helper';
+export * as Utils from './utils';
+export * as ContextHelper from './context-helper';
 
-export { Logger, Command, ApiHelper, GitHelper, Utils, ContextHelper };
+export { Logger, Command, ApiHelper, GitHelper };
