6366fd98d2bd89829eca367292b2154fc52ba761
Technote <technote.space@gmail.com>
2022-04-20 16:19:34 +0900

    chore: add test pattern

---
+++ src/index.test.ts
@@ -0,0 +1,11 @@
+import { expect, it } from 'vitest';
+import { Command, ApiHelper, GitHelper, Types, Utils, ContextHelper } from '.';
+
+it('helpers can be imported', () => {
+  expect(Command).not.toBeFalsy();
+  expect(ApiHelper).not.toBeFalsy();
+  expect(GitHelper).not.toBeFalsy();
+  expect(Types).not.toBeFalsy();
+  expect(Utils).not.toBeFalsy();
+  expect(ContextHelper).not.toBeFalsy();
+});
