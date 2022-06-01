9c720f9000077b7ab63e7843546d077ec81bf069
Technote <technote.space@gmail.com>
2022-06-01 20:58:59 +0900

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import type { Octokit } from './types.js';
 import ApiHelper from './api-helper.js';
 import Command from './command.js';
 import GitHelper from './git-helper.js';
 
-export * as Types from './types.js';
 export * as Utils from './utils.js';
 export * as ContextHelper from './context-helper.js';
 
+export type { Octokit };
 export { Command, ApiHelper, GitHelper };
