74a12cf34721bf220c0b943abd5925f2a6de5bbf
Technote <technote.space@gmail.com>
2023-05-30 00:22:57 +0900

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import type { Octokit, components } from './types.js';
+import crypto from 'crypto';
 import ApiHelper from './api-helper.js';
 import Command from './command.js';
 import GitHelper from './git-helper.js';
+
+if (typeof global.crypto !== 'object') {
+  global.crypto = crypto;
+}
+
+if (typeof global.crypto.getRandomValues !== 'function') {
+  global.crypto.getRandomValues = getRandomValues;
+}
+
+function getRandomValues(array) {
+  return crypto.webcrypto.getRandomValues(array);
+}
 
 export * as Utils from './utils.js';
 export * as ContextHelper from './context-helper.js';
